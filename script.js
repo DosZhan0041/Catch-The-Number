44732de56c7b96ce58fbfc0db6ec71dda0bc1ba6
Doszhan <dosikiliasov@gmail.com>
2024-08-14 12:46:49 +0500

    first deploy

---
+++ script.js
@@ -0,0 +1,82 @@
+
+
+
+// Для изменение цвета фона с помощью кнопки
+
+document.getElementById('button').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'black'
+})
+
+
+// Получить элемент с помощью айдишки
+getElementById("ID")
+
+// Получить элемент с помощью класса 
+getElementsByClassName("class")
+
+// Получить элемент через тэг
+getElementsByTagName('Tag - h1')
+
+// Слушатель событий
+// addEventListener('click', функцию который будет работать при движении)
+
+// добавить что то внутри html
+document.createElement('h1').innerHTML
+
+// Создание либо изменение элемента в head
+document.head
+
+
+// Узнать ширину и высоту экрана клиента
+ let a = document.documentElement.clientHeight
+
+
+// Добавить элемент куда то
+let newItem = document.createElement('item1')
+
+document.getElementById('nav').appendChild(newItem)
+
+
+// Для удаление
+document.getElementById('nav').remove()
+
+// Узнать первый детеныш
+document.getElementById('nav').firstChild
+
+// Узнать последний детеныш
+document.getElementById('nav').lastChild
+
+
+
+
+
+
+history.back()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
